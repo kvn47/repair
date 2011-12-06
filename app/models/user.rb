d6426bf69b4211982cb6e0f997aa8a6f5ec99053
kvn47 <kvn@itechdevs.com>
2011-12-06 12:31:43 +1100

    sessions fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   private
   
     def encrypt_password
-      self.salt = make_salt if new_record?
+      self.salt = make_salt# if new_record?
       self.encrypted_password = encrypt(password) if !(password.blank? && !new_record?)
     end
     
