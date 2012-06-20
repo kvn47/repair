180a6db1923eee004057488ef3d7d09b2ae4be98
kvn47 <kvn@itechdevs.com>
2012-06-20 19:17:41 +1100

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,12 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, params.merge(sort: column, direction: direction, page: nil), {class: css_class, remote: false}
+    dir = (column == sort_column) ? direction : 'both'
+    link_to params.merge(sort: column, direction: direction, page: nil), {class: css_class, remote: false} do
+      (title +
+      image_tag("sort_#{dir}.png")).html_safe
+    end
+    # link_to title, params.merge(sort: column, direction: direction, page: nil), {class: css_class, remote: false}
   end
   
 end
